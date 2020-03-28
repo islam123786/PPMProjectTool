878d9b73007ac5073aa9088b450cb4865d3ca528
Mohammed Siddiqui <mohammed.siddiqui@leicabiosystems.com>
2020-03-28 14:02:34 +0530

    Added Repository

---
+++ pmt/src/main/java/com/danish/pmt/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.danish.pmt.repositories;
+
+import com.danish.pmt.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
