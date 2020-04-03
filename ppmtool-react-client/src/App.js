37e268a0d7fb3ebd8a609ced81983954e672b7f2
Mohammed Siddiqui <mohammed.siddiqui@leicabiosystems.com>
2020-04-03 14:45:49 +0530

    Initial commit for React App

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import React, { Component } from "react";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Dashboard />
+      </div>
+    );
+  }
+}
+
+export default App;
